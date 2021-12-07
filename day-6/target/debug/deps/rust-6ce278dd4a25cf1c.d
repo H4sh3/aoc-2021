60c2c3026d0bfdeb1f3465c27313e4118540ec8d
daniel <pinkdaniel@web.de>
2021-12-07 11:28:59 +0100

    change folder structure

---
+++ day-6/target/debug/deps/rust-6ce278dd4a25cf1c.d
@@ -0,0 +1,5 @@
+/home/daniel/code/sideprojects/aoc-2021/day-6/target/debug/deps/rust-6ce278dd4a25cf1c: src/main.rs
+
+/home/daniel/code/sideprojects/aoc-2021/day-6/target/debug/deps/rust-6ce278dd4a25cf1c.d: src/main.rs
+
+src/main.rs:
---
+++ day-6/target/debug/rust.d
@@ -0,0 +1,1 @@
+/home/daniel/code/sideprojects/aoc-2021/day-6/target/debug/rust: /home/daniel/code/sideprojects/aoc-2021/day-6/src/main.rs
